75c17d972cd3b4515e36db013844760345a208c6
ilanmimoun <ilan.mimoun@sivan-innovation.com>
2018-04-14 21:55:08 +0300

    Fix heroku error: collect static.

--- superlists/settings.py
+++ superlists/settings.py
@@ -88,3 +88,14 @@ STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
 # django_heroku.settings(locals())
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
